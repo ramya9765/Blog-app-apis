8582946d1c6c035222e8477f07e7ebcd42f02be9
10impanad <impanad162@gmail.com>
2024-11-08 18:05:10 +0530

    save user end point created

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/save")
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+	
 }
